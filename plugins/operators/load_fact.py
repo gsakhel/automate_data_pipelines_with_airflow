c4e891d1e1f3e119f68dbd3bc205ce57cf953964
forest <livelyforest@protonmail.com>
2022-05-10 02:21:21 -0700

    Update load fact operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -6,6 +6,7 @@ from airflow.contrib.hooks.aws_hook import AwsHook
 class LoadFactOperator(BaseOperator):
     """Operator for building our fact table
     """
+    
     ui_color = '#F98866'
 
     @apply_defaults
@@ -14,14 +15,10 @@ class LoadFactOperator(BaseOperator):
                  # Example:
                  # conn_id = your-connection-name
                  redshift_conn_id="",
-                 aws_credentials_id="",
+                 #aws_credentials_id="",
                  table="",
-                 s3_bucket="",
-                 s3_key="",
                  create_table_sql="",
-                 insert_table_sql="",
-                 json_settings='auto',
-                 region='us-west-2',
+                 table_insert_sql="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
@@ -29,26 +26,25 @@ class LoadFactOperator(BaseOperator):
         # Example:
         # self.conn_id = conn_id
         self.redshift_conn_id=redshift_conn_id
-        self.aws_credentials_id=aws_credentials_id
+        #self.aws_credentials_id=aws_credentials_id
         self.table=table
-        self.s3_bucket=s3_bucket
-        self.s3_key=s3_key
         
         # Do we need both?
         self.create_table_sql=create_table_sql
-        self.insert_table_sql=insert_table_sql
-        self.json_settings=json_settings
-        self.region=region
+        self.table_insert_sql=table_insert_sql
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
-        aws_hook = AwsHook(self.aws_credentials_id)
-        credentials = aws_hook.get_credentials()
+        self.log.info('LoadFactOperator implemented')
+        
+        # Are these even necessary?
+        #aws_hook = AwsHook(self.aws_credentials_id)
+        #credentials = aws_hook.get_credentials()
 
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info(f"Drop then create table: {self.table}")
-        redshift.run(f"DROP IF EXISTS {self.table}")
+        redshift.run(f"DROP TABLE IF EXISTS public.{self.table}")
         redshift.run(self.create_table_sql)
 
         self.log.info(f"Insert data into table: {self.table}")
-        redshift.run(self.insert_table_sql)
+        redshift.run(self.table_insert_sql)
+        self.log.info(f"Finished Inserting to {self.table}")
