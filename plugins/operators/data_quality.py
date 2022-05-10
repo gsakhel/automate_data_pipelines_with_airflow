d8585f8ceb35196d6e3b8b27843b3ce45871078a
forest <livelyforest@protonmail.com>
2022-05-09 22:14:07 -0700

    Update queries and data_quality

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -11,12 +11,28 @@ class DataQualityOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 self.redshift_conn_id=redshift_conn_id
+                 self.aws_credentials_id=aws_credentials_id
+                 self.table=table
+                 self.s3_bucket=s3_bucket
+                 self.s3_key=s3_key
+                 self.create_table_sql=create_table_sql
+                 self.json_settings=json_settings
+                 self.region=region                 
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.table=table
+        self.s3_bucket=s3_bucket
+        self.s3_key=s3_key
+        self.create_table_sql=create_table_sql
+        self.json_settings=json_settings
+        self.region=region
 
     def execute(self, context):
         self.log.info('DataQualityOperator not implemented yet')
